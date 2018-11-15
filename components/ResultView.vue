9f38a4aa7ce070419bd02fd69e196d468b07f0bf
Lukas Zimmermann <luk.zim91@gmail.com>
2018-11-15 17:05:24 +0100

    Fake data for result view

--- components/ResultView.vue
+++ components/ResultView.vue
@@ -7,19 +7,41 @@
       fullscreen
       hide-overlay
       transition="dialog-bottom-transition">
+
       <v-card>
-        <v-toolbar
-          dark
-          color="primary">
-          <v-btn
-            icon
+        <v-flex xs12>
+          <v-toolbar
             dark
-            @click="$emit('onClose')">
-            <v-icon>close</v-icon>
-          </v-btn>
-          <v-toolbar-title>Results</v-toolbar-title>
-          <v-spacer/>
-        </v-toolbar>
+            color="primary">
+            <v-btn
+              icon
+              dark
+              @click="$emit('onClose')">
+              <v-icon>close</v-icon>
+            </v-btn>
+            <v-toolbar-title>Results</v-toolbar-title>
+            <v-spacer/>
+          </v-toolbar>
+        </v-flex>
+        <v-flex xs12>
+
+          <pre class="preformatted">
+
+
+
+
+                Number of ICD-10 diagnoses for each state:
+
+                Baden-Württemberg: 892
+                Bayern:             15
+                Berlin:              2
+                Hessen:              3
+                Niedersachsen:       5
+                Nordrhein-Westfalen: 9
+                Rheinland-Pfalz:     4
+                Thüringen:           1
+          </pre>
+        </v-flex>
       </v-card>
     </v-dialog>
   </v-layout>
@@ -43,3 +65,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.preformatted {
+  font-size: 1.5em;
+}
+</style>
